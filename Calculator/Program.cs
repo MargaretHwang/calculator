4016b3d9c930f5238a1ec2ef9038ffccf40d1e90
Margaret <bochkovskayamargarita@gmail.com>
2023-01-27 17:00:08 +0200

    initial

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("введите первое число");
+            double numberOne = InputCheck();
+            Console.WriteLine("введите действие");
+            string action = Console.ReadLine();
+            Console.WriteLine("введите второе число");
+            double numberTwo = InputCheck();
+            Actions(action, numberOne, numberTwo);
+        }
+    }
+    static void Actions(string action, double numberOne, double numberTwo)
+    {
+        if (action == "+")
+            Console.WriteLine("Result: " + (numberOne + numberTwo));
+        if (action == "-")
+            Console.WriteLine("Result: " + (numberOne - numberTwo));
+        if (action == "*")
+            Console.WriteLine("Result: " + (numberOne * numberTwo));
+        if (action == "/")
+        
+            if (numberTwo == 0)
+                Console.WriteLine("Error,you can't divide by 0");
+            else
+                Console.WriteLine("Result: " + (numberOne / numberTwo));
+        
+
+    }
+
+    static double InputCheck()
+    {
+        while(true)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                return number;
+            }
+            catch
+            {
+                Console.WriteLine("it's not a number!!!!!!!!! Try again:)");
+            }
+        }
+    }
+}
